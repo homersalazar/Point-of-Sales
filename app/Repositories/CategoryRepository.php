818c3e2cddd56e27625ae434474b2a98860e6e91
Homer Salazar <salazarhomer1@gmail.com>
2026-02-18 15:22:05 +0800

    chore (CategoryRepository, ProductRepository): add baseRepository to avoid redundancy

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -4,44 +4,21 @@ namespace App\Repositories;
 
 use App\Models\Category;
 
-class CategoryRepository
+class CategoryRepository extends BaseRepository
 {
+    public function __construct(Category $model)
+    {
+        parent::__construct($model);
+    }
+
     public function paginate($search = null, $perPage = 10)
     {
-        return Category::when($search, function ($query) use ($search) {
+        return $this->model->when($search, function ($query) use ($search) {
             $query->where('name', 'like', "%{$search}%")
                 ->orWhere('description', 'like', "%{$search}%");
         })
             ->orderBy('name', 'asc')
             ->paginate($perPage)
             ->appends(['search' => $search]);
-    }
-
-    public function all()
-    {
-        return Category::latest()->get();
-    }
-
-    public function create(array $data)
-    {
-        return Category::create($data);
-    }
-
-    public function find($id)
-    {
-        return Category::findOrFail($id);
-    }
-
-    public function update(array $data, $id)
-    {
-        $category = $this->find($id);
-        $category->update($data);
-        return $category;
-    }
-
-    public function delete($id)
-    {
-        $category = $this->find($id);
-        $category->delete();
     }
 }
--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -4,11 +4,16 @@ namespace App\Repositories;
 
 use App\Models\Product;
 
-class ProductRepository
+class ProductRepository extends BaseRepository
 {
+    public function __construct(Product $model)
+    {
+        parent::__construct($model);
+    }
+
     public function paginate($search = null, $perPage = 10)
     {
-        return Product::when($search, function ($query) use ($search) {
+        return $this->model->when($search, function ($query) use ($search) {
             $query->where('name', 'like', "%{$search}%")
                 ->orWhere('cost_price', 'like', "%{$search}%")
                 ->orWhere('selling_price', 'like', "%{$search}%")
@@ -17,33 +22,5 @@ class ProductRepository
             ->orderBy('name', 'asc')
             ->paginate($perPage)
             ->appends(['search' => $search]);
-    }
-
-    public function all()
-    {
-        return Product::latest()->get();
-    }
-
-    public function create(array $data)
-    {
-        return Product::create($data);
-    }
-
-    public function find($id)
-    {
-        return Product::findOrFail($id);
-    }
-
-    public function update(array $data, $id)
-    {
-        $product = $this->find($id);
-        $product->update($data);
-        return $product;
-    }
-
-    public function delete($id)
-    {
-        $product = $this->find($id);
-        $product->delete();
     }
 }
